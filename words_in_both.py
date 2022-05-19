4c31c5b15b305367031ac31f6094b2fc4deadb0a
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-18 22:21:51 -0700

    update

--- words_in_both.py
+++ words_in_both.py
@@ -1,13 +1,16 @@
 # Author: Christopher Boyd
 # GitHub username: CtheBoyd
 # Date: 5/18/2022
-# Description: Asks user to provide five numbers and then provides an average.
+# Description: Takes two strings and returns words that are in both strings.
 #
 
-def words_in_both(a, b):
-    a1 = set(a.lower().split())
-    b1 = set(b.lower().split())
-    return a1.intersection(b1)
+def words_in_both(sent_1, sent_2):
+    """Takes two strings and returns words that are in both strings."""
+    sent_1 = set(sent_1.lower().split())
+
+    sent_2 = set(sent_2.lower().split())
+
+    return sent_1.intersection(sent_2)
 
 common_words = words_in_both("She is a jack of all trades", 'Jack was tallest of all')
 print(common_words)
